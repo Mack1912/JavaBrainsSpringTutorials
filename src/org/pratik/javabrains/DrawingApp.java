bce1f5d6954783d3fe2cb66bd7f7c3ca0602bb05
Mack <pratikbongale1912@gmail.com>
2017-08-26 20:09:17 -0400

    using InitialingBean and DisposableBean interface to add myInit and cleanUp methods

--- src/org/pratik/javabrains/DrawingApp.java
+++ src/org/pratik/javabrains/DrawingApp.java
@@ -1,14 +1,18 @@
 package org.pratik.javabrains;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {
 
         // its and interface that reads the blueprint of configuration from a xml file
-        BeanFactory factory = new ClassPathXmlApplicationContext("spring.xml");
-        Triangle t = (Triangle) factory.getBean("triangle1");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        // important if we are overriding the destroy() method of DisposableBean interface
+        context.registerShutdownHook();
+        Triangle t = (Triangle) context.getBean("triangle");
         t.draw();
     }
 
--- src/org/pratik/javabrains/Triangle.java
+++ src/org/pratik/javabrains/Triangle.java
@@ -1,28 +1,73 @@
 package org.pratik.javabrains;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 import java.util.List;
 
-public class Triangle {
+public class Triangle implements InitializingBean, DisposableBean {
 
-    List<Point> points;
+//    List<Point> points;
+    Point p1;
+    Point p2;
+    Point p3;
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getP1() {
+        return p1;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
+    public Point getP3() {
+        return p3;
+    }
+
+    public void setP3(Point p3) {
+        this.p3 = p3;
     }
 
     public void draw() {
-//        System.out.println(pt1);
-//        System.out.println(pt2);
-//        System.out.println(pt3);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
 
-        for (Point p : points
-             ) {
-            System.out.println(p);
-        }
+//        for (Point p : points
+//             ) {
+//            System.out.println(p);
+//        }
 
+    }
+
+    // this method gets priority
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Destroying by the interface method: destroy");
+    }
+
+
+    // this method gets priority
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing by the interface method: afterPropertiesSet");
+    }
+
+    // myInit is a standard name used for init methods
+    public void myInit() {
+        System.out.println("Init method from the spring config file called");
+    }
+
+    // cleanUp is a standard name used for destroy methods
+    public void cleanUp() {
+        System.out.println("cleanUp method from the spring config file called");
     }
 }
